b551b0bb8be955df1e4eebcca2adbe0ab63a412f
Elmar Schug <elmar.schug@jayware.org>
2016-04-22 18:47:38 +0200

    Added code to the solid-utilities project.

---
+++ src/main/java/org/jayware/solid/utilities/Key.java
@@ -0,0 +1,78 @@
+/**
+ * Skyshard Engine -- A 3D game engine written in Java
+ *
+ * Copyright (C) 2016 Markus Neubauer <markus.neubauer@jayware.org>,
+ *                    Alexander Haumann <alexander.haumann@jayware.org>,
+ *                    Manuel Hinke <manuel.hinke@jayware.org>,
+ *                    Marina Schilling <marina.schilling@jayware.org>,
+ *                    Elmar Schug <elmar.schug@jayware.org>,
+ *
+ *     This file is part of the Skyshard Engine.
+ *
+ *     The Skyshard Engine is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *     the License, or any later version.
+ *
+ *     The Skyshard Engine is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jayware.e2.util;
+
+
+import java.util.Objects;
+
+
+/**
+ * A <code>Key</code>
+ *
+ * @param <V> the corresponding value type.
+ *
+ * @since 1.0
+ */
+public class Key<V>
+{
+    private final String myKey;
+
+    private Key(String key)
+    {
+        myKey = key;
+    }
+
+    public static <V> Key<V> createKey(String key)
+    {
+        return new Key(key);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        final Key<?> key = (Key<?>) o;
+        return Objects.equals(myKey, key.myKey);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(myKey);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Key{ " + myKey + " }";
+    }
+}
---
+++ src/main/java/org/jayware/solid/utilities/Preconditions.java
@@ -0,0 +1,149 @@
+/**
+ * Skyshard Engine -- A 3D game engine written in Java
+ *
+ * Copyright (C) 2016 Markus Neubauer <markus.neubauer@jayware.org>,
+ *                    Alexander Haumann <alexander.haumann@jayware.org>,
+ *                    Manuel Hinke <manuel.hinke@jayware.org>,
+ *                    Marina Schilling <marina.schilling@jayware.org>,
+ *                    Elmar Schug <elmar.schug@jayware.org>,
+ *
+ *     This file is part of the Skyshard Engine.
+ *
+ *     The Skyshard Engine is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *     the License, or any later version.
+ *
+ *     The Skyshard Engine is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jayware.e2.util;
+
+
+/**
+ * Static convenience methods that help a method or constructor checkArgument whether it was invoked correctly.
+ * <p>
+ * <b>Note:</b> These methods are based on methods of the {@link com.google.common.base.Preconditions Google Guava Preconditions}
+ * class.
+ */
+public class Preconditions
+{
+    @FunctionalInterface
+    public interface Precondition
+    {
+        boolean check();
+    }
+
+    /**
+     * Ensures that the specified {@link Precondition} is met.
+     * <p>
+     * If the passed {@link Precondition} returns <code>false</code> an {@link IllegalArgumentException} is thrown.
+     *
+     * @param precondition a {@link Precondition}
+     *
+     *
+     * @throws IllegalArgumentException if the specified {@link Precondition} returns false.
+     */
+    public static void checkArgument(Precondition precondition)
+    {
+        if (!precondition.check())
+        {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Ensures that the specified {@link Precondition} is met.
+     * <p>
+     * If the passed {@link Precondition} returns <code>false</code> an {@link IllegalStateException} is thrown.
+     *
+     * @param precondition a {@link Precondition}
+     *
+     *
+     * @throws IllegalArgumentException if the specified {@link Precondition} returns false.
+     */
+    public static void checkState(Precondition precondition)
+    {
+        if (!precondition.check())
+        {
+            throw new IllegalStateException();
+        }
+    }
+
+    /**
+     * Ensures that an object reference passed as a parameter to the calling method is not null.
+     * <p>
+     * <b>Note:</b> This operation is based on the {@link com.google.common.base.Preconditions#checkNotNull(Object)
+     * Google Guavas Preconditions},
+     * but instead of throwing a {@link NullPointerException} when the passed reference is null, this operation
+     * throws an {@link IllegalArgumentException}.
+     *
+     * @param reference an object reference
+     * @param <T> the object's type.
+     *
+     * @return the non-null reference that was validated
+     *
+     * @throws IllegalArgumentException if reference is <code>null</code>
+     */
+    public static <T> T checkNotNull(T reference)
+    {
+        if (reference == null)
+        {
+            throw new IllegalArgumentException();
+        }
+        return reference;
+    }
+
+    /**
+     * Ensures that an object reference passed as a parameter to the calling method is not null.
+     * <p>
+     * <b>Note:</b> This operation is based on the {@link com.google.common.base.Preconditions#checkNotNull(Object, Object)
+     * Google Guavas Preconditions},
+     * but instead of throwing a {@link NullPointerException} when the passed reference is null, this operation
+     * throws an {@link IllegalArgumentException}.
+     *
+     * @param reference    an object reference
+     * @param errorMessage the exception message to use if the checkArgument fails; will be converted to a
+     *                     string using {@link String#valueOf(Object)}
+     * @param <T> the object's type.
+     * @return the non-null reference that was validated
+     *
+     * @throws IllegalArgumentException if reference is <code>null</code>
+     */
+    public static <T> T checkNotNull(T reference, Object errorMessage)
+    {
+        if (reference == null)
+        {
+            throw new IllegalArgumentException(String.valueOf(errorMessage));
+        }
+        return reference;
+    }
+
+    /**
+     * Ensures that a {@link String} passed as a parameter to the calling method is not empty.
+     * <p>
+     * <b>Note:</b> This operation is based on the {@link com.google.common.base.Preconditions#checkNotNull(Object)
+     * Google Guavas Preconditions},
+     * but instead of throwing a {@link NullPointerException} when the passed reference is null, this operation
+     * throws an {@link IllegalArgumentException}.
+     *
+     * @param string a {@link String}
+     *
+     * @return the {@link String} that was validated
+     *
+     * @throws IllegalArgumentException if {@code string} is null
+     */
+    public static String checkStringNotEmpty(String string)
+    {
+        if (string == null || string.isEmpty())
+        {
+            throw new IllegalArgumentException();
+        }
+        return string;
+    }
+}
